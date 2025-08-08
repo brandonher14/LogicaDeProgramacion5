c06e4330f02138b05b24d9625610a717a069b497
brandon Hernandez <brandonpais14@gmail.com>
2025-08-08 17:28:59 -0600

    Avance final, se aniade lo faltante asi como un resutlado al final de correctas y equivocadas

--- src/CatalogoDiccionario.java
+++ src/CatalogoDiccionario.java
@@ -1,10 +1,11 @@
 import java.util.*;
 
 public class CatalogoDiccionario {
-    private final Map<String, String> diccionario;
 
-    public CatalogoDiccionario() {
-        diccionario = new HashMap<>();
+    public static void main(String[] args) {
+
+
+     HashMap<String, String> diccionario = new HashMap<>();
         diccionario.put("gato", "cat");
         diccionario.put("perro", "dog");
         diccionario.put("casa", "house");
@@ -25,17 +26,42 @@ public class CatalogoDiccionario {
         diccionario.put("niño", "child");
         diccionario.put("ciudad", "city");
         diccionario.put("música", "music");
+
+
+    List<String> claves = new ArrayList<>(diccionario.keySet());
+        Collections.shuffle(claves); // Mezclar aleatoriamente
+    List<String> cincoPalabras = claves.subList(0, 5);
+    //Instruccion al usuario
+        System.out.println("Traduce al inglés las siguientes palabras en español:");
+        for (String palabra : cincoPalabras) {
+            System.out.println("- " + palabra);
+        }
+        Scanner sc = new Scanner(System.in);
+        int correctas = 0;
+        int incorrectas = 0;
+
+        for (String palabraEsp : cincoPalabras) {
+            System.out.print("Traducción al inglés de '" + palabraEsp + "': ");
+            String respuestaUsuario = sc.nextLine().trim();
+
+            String traduccionCorrecta = diccionario.get(palabraEsp);
+
+            if (respuestaUsuario.equalsIgnoreCase(traduccionCorrecta)) {
+                System.out.println("Correcto");
+                correctas++;
+            } else {
+                System.out.println("Incorrecto");
+                incorrectas++;
+            }
+        }
+
+        // Resultados
+        System.out.println("\nResultados:");
+        System.out.println("correctas: " + correctas);
+        System.out.println(" incorrectas: " + incorrectas);
     }
-
-    public List<String> obtenerClavesAleatorias() {
-        List<String> claves = new ArrayList<>(diccionario.keySet());
-        Collections.shuffle(claves);
-        List<String> palabrasEscogidas = claves.subList(0,5);
-        return claves;
-    }
-    System.out.println("Traduce las siguientes palabras al ingles: ");
-    for (String)
-
-
 }
+
+
+
 
